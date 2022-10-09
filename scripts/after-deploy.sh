9b8384b5fc1308670298806054183397a8486102
westkite1201 <westkite1201@gmail.com>
2022-10-09 20:38:18 +0900

    🔨 [FIX] : after-deploy 빌드 파일 복사 이동 추가

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 PROJECT_PATH="/home/ubuntu/seo/gitrepos/IIDT-backend"
-cd $REPOSITORY
 
-sudo yarn
+SERVICE_PID=`ps -ef | grep -ie "-p 3031"  | grep -v grep | awk '{print $2}'`
+APPLICATION_NAME='ifs'
 
-sudo ./start.sh
+# 기존 폴더 삭제 후 재 생성 
+rm -rf ${PROJECT_PATH}/bulid
+mkdir ${PROJECT_PATH}/bulid
+
+ # data 복사 
+cp -r  /home/ubuntu/bulid/bulid  ${PROJECT_PATH}/bulid
+    
+
+cd ${PROJECT_PATH}
+
+# docker script 실행 
+./start.sh
+
+echo
+echo "==[${APPLICATION_NAME}] process start"
+
